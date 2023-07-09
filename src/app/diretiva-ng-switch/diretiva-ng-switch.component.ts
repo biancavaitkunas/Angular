883c97200dab8441b1f2efca79383d10236ea610
Bianca <biancavaitkunas@gmail.com>
2023-07-09 20:35:16 -0300

    Commit

--- src/app/diretiva-ng-switch/diretiva-ng-switch.component.ts
+++ src/app/diretiva-ng-switch/diretiva-ng-switch.component.ts
@@ -6,6 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretiva-ng-switch.component.scss']
 })
 export class DiretivaNgSwitchComponent {
-  public valor: number = 1;
+  public cor: string = '';
+  /*corEscolhida(){
+    this.cor = this.trocaCor('rosa')
+  }*/
+  
+
+  public trocaCor(escolhida: string){
+    var cor = '';
+
+    if (cor = "rosa") {
+      cor = '#ff0000';
+    }
+    if (cor = "azul") {
+      cor = '#00ff00';
+    }
+    if (cor = "vermelho") {
+      cor = '#FFFF00';
+    }
+    if (cor = "amarelo") {
+      cor = '#ff0000';
+    }
+    if (cor = "roxo") {
+      cor = '#ff0000';
+    }
+    if (cor = "verde") {
+      cor = '#ff0000';
+    }
+    if (cor = "branco") {
+      cor = '#ff0000';
+    }
+    if (cor = "preto") {
+      cor = '#fffff';
+    }
+    return cor;
+  }
 
 }
+
--- src/app/exemplo-microondas/exemplo-microondas.component.ts
+++ src/app/exemplo-microondas/exemplo-microondas.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./exemplo-microondas.component.scss']
 })
 export class ExemploMicroondasComponent {
+  tempoDefinido: number = 0;
+  tempoRestante: number = 0;
+  timer: any;
+
+  public iniciar(){
+    if (this.tempoDefinido > 0 && !this.timer) {
+      this.tempoRestante = this.tempoDefinido;
+      this.timer = setInterval(() => {
+        if (this.tempoRestante > 0) {
+          this.tempoRestante--;
+        } else {
+          this.cancelar();
+          console.log('Fim!');
+        }
+      }, 1000);
+    }
+  }
+
+  public cancelar() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  public zerar() {
+    this.cancelar();
+    this.tempoDefinido = 0;
+    this.tempoRestante = 0;
+  }
+
+  public definirTempo(tempo: number) {
+    if (!this.timer) {
+      this.tempoDefinido = tempo;
+    }
+  }
 
 }
